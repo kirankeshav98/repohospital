9d64d0b4c0e91d77f1b39295748e52fb92b9f8f4
kirankeshav98 <kirankeshavpk1998@gmail.com>
2023-02-19 13:23:27 +0530

    added

---
+++ home/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from . models import Booking
+
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
+class BookingForm(forms.ModelForm):
+    class Meta:
+        model = Booking
+        fields = '__all__'
+        widgets = {
+            'booking_date':DateInput()
+        }
+
+        labels = {
+            'p_name':'Patient Name',
+            'p_phone':'Contact Number',
+            'p_email':'Email id',
+            'doc_name':'Doctor Name',
+        }
